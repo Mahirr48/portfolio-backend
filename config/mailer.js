3cfa0d7c9a59d28da9dd72f6a30a31ea506dbd67
Mahirr48 <pathanmahir736@gmail.com>
2026-04-14 17:00:11 +0530

    fix: Chnaged The Transporter

--- config/mailer.js
+++ config/mailer.js
@@ -1,26 +1,11 @@
 import nodemailer from "nodemailer";
-import dns from "dns";
-
-// ✅ FORCE IPv4 (THIS FIXES YOUR ERROR)
-dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  family: 4, // ✅ force IPv4
+  service: "gmail", // 🔥 SWITCH THIS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});
-
-transporter.verify((error, success) => {
-  if (error) {
-    console.error("SMTP ERROR:", error);
-  } else {
-    console.log("SMTP READY");
-  }
 });
 
 export default transporter;
